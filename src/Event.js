b7ec3b349792b3d4bfc905f713dc71b8e6694a86
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-05-10 15:36:51 -0600

    fill skeleton

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state= { collapsed: true }
+  
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      collapsed: !prevState.collapsed,
+    }));
+  };
+  
   render() {
-    return <div></div>;
-  }
+    const { event } = this.props;
+    const { collapsed } =this.state;
+    return (
+    <div>
+      <h2 className="summary">{event.summary}</h2>
+      <div className="startTime">{new Date(event.start.dateTime).toString()}</div>
+      <div className="location">{event.location}</div>
+      {!collapsed && (
+      <div className='details'>
+        <div className="description">{event.description}</div>
+        <a href={event.htmlLink}>See Details in Google Calendar</a>
+      </div>
+      )}
+      <button className='toggle-details' onClick={this.toggleDetails}>Details</button>
+    </div>
+  )};
 }
+
+// console.log(summary);
 export default Event;
