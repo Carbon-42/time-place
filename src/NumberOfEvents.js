8c56f34f400f5b29f8684d48afb9618220695a76
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-06-14 13:52:01 -0600

    add ErrorAlert object

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state={
-    value: 36
+    value: 36,
+    errorAlert: ''
   }
   
 
   handleClick = event => {
     const value = event.target.value;
-    this.setState({value});
-    this.props.updateEvents(this.props.selectedCity, value);
+    console.log(value);
+    if (value === '-1') {
+      this.setState({
+        value, 
+        errorAlert: 'This is an invalid number of events'
+      })
+    } else {
+      this.setState({value, errorAlert: ''});
+      this.props.updateEvents(this.props.selectedCity, value);
+    }
   };
 
   render() {
     return (
       <div className="selector">
+        <ErrorAlert text={this.state.errorAlert} />
         <label className="number">Number of Events: </label>
         <select 
           id="number"
@@ -26,6 +37,7 @@ class NumberOfEvents extends Component {
           <option id="option2" value="24">24</option>
           <option id="option3" value="12">12</option>
           <option id="option4" value="6">6</option>
+          <option id="option5" value="-1">-1 for exc 4.8 alert testing only</option>          
         </select>
       </div>
 
